8be35e8a6b22d6eed0ff7fdbd31765946130ba59
Lizard Master <151883713+TheLizardMaster@users.noreply.github.com>
2024-01-03 18:19:34 +0000

    Completed Exercise 1 and 2. All tests passing.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
 
+    let repeatedString = string;
+
+        for (let i=0; i < (num-1); i++) {
+            repeatedString += string;
+        }
+
+        if (num > 0) {
+
+            return repeatedString;
+
+        } else if (num < 0) {
+
+            return "ERROR";
+            
+        } else return "";
+        
 };
 
 // Do not edit below this line
