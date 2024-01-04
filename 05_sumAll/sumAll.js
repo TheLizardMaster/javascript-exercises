1ae935863a33aece6ba365b06be271ea8db275de
Lizard Master <151883713+TheLizardMaster@users.noreply.github.com>
2024-01-04 15:44:12 +0000

    Completed exercise 5 and 6. All tests passed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,31 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+
+    let sum = 0;
+
+    if ((a || b) < 0) {
+
+        return "ERROR";
+
+    } else if (!Number.isInteger(a) || !Number.isInteger(b)) {
+
+        return "ERROR"
+
+    } else if (a > b) {
+
+        for (let i = b; i <= a; i++) {
+            sum += i;
+          }
+          return sum;
+
+
+    } else if (b > a) {
+
+        for (let i = a; i <= b; i++) {
+            sum += i;
+          }
+          return sum;
+
+    } else return "ERROR";
 
 };
 
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,8 @@
-const leapYears = function() {
+const leapYears = function(years) {
+
+    if ((years % 100 === 0 && years % 400 !== 0) || years % 4 !== 0){
+        return false;
+    } else return true; 
 
 };
 
