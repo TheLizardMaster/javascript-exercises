c678118cc7cad81e0d5dd723447eac55e771db2f
Lizard Master <151883713+TheLizardMaster@users.noreply.github.com>
2024-01-04 17:06:21 +0000

    Completed exercise 7 and all tests passed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faren) {
+    const number = (faren -32) * (5/9);
+    return Math.round(number * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+    const number = (cels * (9/5)) + 32;
+    return Math.round(number * 10) / 10;
 };
 
 // Do not edit below this line
