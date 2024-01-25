80e5685aa02d6571aef8905dd1cfe46e39c42b0f
Lizard Master <151883713+TheLizardMaster@users.noreply.github.com>
2024-01-25 22:05:33 +0000

    Completed exercise 8. All tests passed.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+
+	    const sumArray = arr.reduce((total, value) =>  total + value, 0);
+      return sumArray;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+
+      const multArray = arr.reduce((total, value) =>  total * value, 1);
+      return multArray;
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  for (let i = num -1; i >= 1; i--){
+    num *= i;
+  }
+
+  return num;
 };
 
 // Do not edit below this line
