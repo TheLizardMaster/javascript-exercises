be7f01464ab5f2c0074b7af87ffe3d203c89378b
Lizard Master <151883713+TheLizardMaster@users.noreply.github.com>
2024-01-28 00:27:34 +0000

    Completed exercise 9. All tests passed.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (pal) {
+
+    let palArray = pal.split('').filter(char => /[a-zA-Z0-9]/.test(char));
+    const forwardJoin = palArray.join('');
+
+    const revArray = palArray.reverse();
+    const joinedArray = revArray.join('');
+
+    if (joinedArray.toUpperCase() === forwardJoin.toUpperCase()) {
+        return true;
+    } else return false;
 
 };
 
