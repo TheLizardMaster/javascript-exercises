7b453315128099dd31a9de4f5fa0d87a778d9eee
Lizard Master <151883713+TheLizardMaster@users.noreply.github.com>
2024-02-13 16:29:13 +0000

    Completed exercise 10. All tests passed.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
 
+    let nacci = Math.floor(((1 + Math.sqrt(5))**fib - (1 - Math.sqrt(5))**fib) / (2**fib * Math.sqrt(5)));
+
+    if (fib < 0) {
+        return "OOPS";
+    } else {
+        return nacci;
+    }
 };
 
 // Do not edit below this line
