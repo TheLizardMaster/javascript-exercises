1be799eb43e70fdb1259d94c4598fada5654ca20
Lizard Master <151883713+TheLizardMaster@users.noreply.github.com>
2024-02-20 17:46:45 +0000

    Completed exercise 11. All tests passed.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookarray) {
+
+    return bookarray.map((book) => book.title);
 
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
