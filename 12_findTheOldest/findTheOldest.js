79f00a89fb0b8ae84125a24a70ea5f5d322de468
Lizard Master <151883713+TheLizardMaster@users.noreply.github.com>
2024-02-23 18:23:16 +0000

    Completed exercise 12. All tests passed.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(personArray) {
 
+    // MAP Method
+
+    const giveDeath = personArray.map((person) => {
+        if (person.yearOfDeath) {
+           return person;
+        } else if (!person.yearOfDeath){
+            person.yearOfDeath = 2024;
+            return person;
+        }
+    });
+    
+    // SORT Method
+    
+    const sortedByOldest = giveDeath.sort((a,b) => {
+    
+        if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth) {
+            return -1;
+        } else {
+            return 1;
+        }
+    
+    })
+
+    return sortedByOldest[0];
 };
 
 // Do not edit below this line
